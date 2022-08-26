a6c6d4d9a610fbe039f260f777001a049ae9532b
Devskiller <owl@devskiller.com>
2022-08-26 13:18:22 +0000

    Candidate repo initialization

---
+++ src/wsServer-api.js
@@ -0,0 +1,12 @@
+const WebSocketServer = require('ws').Server;
+
+module.exports = (stepService) => {
+  const WEBSOCKET_PORT = 8081;
+
+  // * TODO: Write the WebSocket API for receiving `update`s,
+  //         using `stepService` for data persistence.
+  // * TODO: Make sure to return an instance of a WebSocketServer,
+  //         which contains `close()` method.
+
+  return wsServer;
+};
